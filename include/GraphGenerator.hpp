755c3b4b93c5ad3c3a7a8baa4b9c851074fd8ccb
kubam <jexelem@gmail.com>
2026-05-21 22:20:07 +0200

    Define methods for GraphGenerator

--- include/GraphGenerator.hpp
+++ include/GraphGenerator.hpp
@@ -1,1 +1,24 @@
-// TODO: TO jako pierwsze. Rozroznic skierowany i nieskierowany.
+#ifndef GRAPH_GENERATOR_HPP
+#define GRAPH_GENERATOR_HPP
+
+#include <string>
+
+enum class GraphType {
+    DIRECTED,
+    UNDIRECTED
+};
+
+class GraphGenerator {
+public:
+    // Parameters:
+    // - filename
+    // - vertices
+    // - density
+    // - type
+    static void generateAndSave(const std::string& filename, int vertices, double density, GraphType type);
+
+private:
+    static int getRandomWeight(int maxEdges);
+};
+
+#endif
