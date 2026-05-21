432f009fea7d6637fef62e95813a10405887bc6a
kubam <jexelem@gmail.com>
2026-05-21 22:31:02 +0200

    Add interface for graphs

---
+++ include/graph/IGraph.hpp
@@ -0,0 +1,36 @@
+#ifndef IGRAPH_HPP
+#define IGRAPH_HPP
+
+#include "data_structures/Array.hpp"
+#include <iostream>
+#include <iomanip>
+
+struct Edge {
+    int to;
+    int weight;
+};
+
+class IGraph {
+protected:
+    int numVertices;
+    int numEdges;
+    bool isDirected;
+
+public:
+    IGraph(int vertices, bool directed)
+        : numVertices(vertices), numEdges(0), isDirected(directed) {}
+
+    virtual ~IGraph() = default;
+
+    virtual void addEdge(int from, int to, int weight) = 0;
+
+    virtual void getNeighbors(int vertex, DynamicArray<Edge>& neighbors) const = 0;
+
+    virtual void display() const = 0;
+
+    [[nodiscard]] int getVerticesCount() const { return numVertices; }
+    [[nodiscard]] int getEdgesCount() const { return numEdges; }
+    [[nodiscard]] bool getIsDirected() const { return isDirected; }
+};
+
+#endif
